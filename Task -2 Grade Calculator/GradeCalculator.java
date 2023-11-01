53f34c954dc17050dd6953626aaa10c4df717309
Sumeetkumar katke <snkatke9874@gmail.com>
2023-11-01 13:32:45 +0530

    Fixed indentation

--- Task -2 Grade Calculator/GradeCalculator.java
+++ Task -2 Grade Calculator/GradeCalculator.java
@@ -9,84 +9,71 @@ public class GradeCalculator {
         totalmarks = 0;
         sub = subjects;
 
-        for (int i=1; i<= sub; i++){
-            System.out.print("Enter the marks obtained in subject - 0"+i + ": " );
+        for (int i = 1; i <= sub; i++) {
+            System.out.print("Enter the marks obtained in subject - " + i + ": ");
             int marks = input.nextInt();
 
-            if (marks < 0 || marks > 100){
-                System.out.println("Please enter from the range 0 to 100");
+            if (marks < 0 || marks > 100) {
+                System.out.println("Please enter marks within the range 0 to 100");
                 i--;
-            }
-            else{
-
-                totalmarks +=marks;
+            } else {
+                totalmarks += marks;
             }
         }
     }
 
-    public double percent(){
+    public double percent() {
         return (double) totalmarks / sub;
     }
 
-    public String Grade(){
+    public String Grade() {
         String grade;
         double percent = percent();
-        if(percent<=25){
-            grade = "F";            
-        }
-        else if(percent<=35){
+
+        if (percent <= 25) {
+            grade = "F";
+        } else if (percent <= 35) {
             grade = "E";
-        }
-
-        else if (percent<=55){
+        } else if (percent <= 55) {
             grade = "C";
-        }
-
-        else if (percent<=65){
+        } else if (percent <= 65) {
             grade = "B";
-        }
-
-        else if (percent<=75){
+        } else if (percent <= 75) {
             grade = "B+";
-        }
-
-        else if (percent<=85){
+        } else if (percent <= 85) {
             grade = "A";
-        }
-        else if (percent<=95){
+        } else if (percent <= 95) {
             grade = "A+";
-        }
-        else{
+        } else {
             grade = "O";
         }
 
         return grade;
-
     }
 
-    public void showresult(){
-        System.out.print("\nGarde obtained: "+ Grade() +
-                        "\nTotal marks: "+ totalmarks+
-                        "\nPercentage obtained: "+ percent());
-        
+    public void showresult() {
+        System.out.print("\nGrade obtained: " + Grade() +
+                "\nTotal marks: " + totalmarks +
+                "\nPercentage obtained: " + percent() + "%");
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int students ;
+        int students;
         System.out.print("Enter the total Number of students: ");
         students = input.nextInt();
-        int subj;
+        
+        int subjects;
         System.out.print("Enter the total Number of subjects: ");
-        subj = input.nextInt();
+        subjects = input.nextInt();
 
         GradeCalculator[] student = new GradeCalculator[students];
-        for (int i=0; i<students; i++){
+        for (int i = 0; i < students; i++) {
             student[i] = new GradeCalculator();
-            System.out.print("\n\nStudent "+ (i+1)+ ": \n");
-            student[i].inputmarks(subj);
+            System.out.print("\n\nStudent " + (i + 1) + "\n");
+            student[i].inputmarks(subjects); // Pass the total subjects as an argument
             student[i].showresult();
         }
-        input.close();   
+        input.close();
     }
 }
